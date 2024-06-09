33a080504db71b4005d6075daa516a0785d76924
StasX <s.mestechkin@gmail.com>
2024-06-10 00:06:30 +0300

    Remove comments

--- React/src/Services/UserService.ts
+++ React/src/Services/UserService.ts
@@ -10,48 +10,25 @@ class UserService {
 
         const token = localStorage.getItem("token"); 
         if (!token) return;
-        const dbUser = jwtDecode<{ user: UserModel }>(token).user;
-
-        // Create action object containing the logged-in user: 
-        const action = userActions.login(dbUser);
-
-        // Save logged-in user in global state:
+        const user = jwtDecode<{ user: UserModel }>(token).user; 
+        const action = userActions.login(user);
         store.dispatch(action);
     }
 
     public async login(credentials: CredentialsModel): Promise<void> {
-
-        // Send credentials to backend:
         const response = await axios.post<string>(appConfig.loginUrl, credentials);
-
-        // Extract token from axios response: 
-        console.log(response.data);
         const token = response.data;
-
-        // Extract user from token:
-        const dbUser = jwtDecode<{ user: UserModel }>(token).user;
-
-        // Create action object containing the logged-in user: 
-        const action = userActions.login(dbUser);
-
-        // Save logged-in user in global state:
-        store.dispatch(action);
-
-        // Save token to local storage: 
+        const user = jwtDecode<{ user: UserModel }>(token).user; 
+        const action = userActions.login(user);
+        store.dispatch(action); 
         localStorage.setItem("token", token);
     }
 
     public logout(): void {
-        // Create action object for logout:
         const action = userActions.logout();
-
-        // Logout user from global state:
         store.dispatch(action);
-
-        // Remove token from local storage: 
         localStorage.removeItem("token");
     }
-
 }
 
 export const userService = new UserService();
