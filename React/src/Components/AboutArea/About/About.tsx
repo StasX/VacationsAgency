d85a2d959d6a8e7173f31924ce4368939da2129a
StasX <s.mestechkin@gmail.com>
2024-06-09 23:50:29 +0300

    Refactor About

--- React/src/Components/AboutArea/About/About.tsx
+++ React/src/Components/AboutArea/About/About.tsx
@@ -1,31 +1,61 @@
+
 import { useTitle } from "../../../Utils/UseTitle";
+import { Container, Typography, List, ListItem, ListItemText, Grid } from "@mui/material";
 import css from "./About.module.css";
 
 export function About(): JSX.Element {
-    useTitle("About...")
+    useTitle("About...");
+    const frontEndTech: string[] = [
+        "React",
+        "Bootstrap",
+        "JavaScript",
+        "TypeScript",
+        "Material UI"
+    ];
+    const backEndTech: string[] = [
+        "Flask",
+        "Django",
+        "Python",
+        "MySQL",
+        "Docker",
+        "AWS"
+    ];
     return (
-        <div className={css.About}>
-            <h1>About me:</h1>
-            <p>My name is Stanislav Mestechkin, and I am the full stack developer...
-                I have extensive developing web applications, and I am responsible for all stages of the project - from planning and development to testing and technical support.</p>
+        <Container className={css.About} maxWidth="md">
+            <Typography variant="h3" gutterBottom>
+                About me:
+            </Typography>
+            <Typography variant="body1" paragraph>
+                My name is Stanislav Mestechkin, and I am a full stack developer...
+                I have extensive experience developing web applications, and I am responsible for all stages of the project - from planning and development to testing and technical support.
+            </Typography>
 
-            <h2>Tools and Technologies Used in current project:</h2>
-            <ul>
-                Front End:
-                <li>React</li>
-                <li>Bootstrap</li>
-                <li>JavaScript</li>
-                <li>TypeScript</li>
-            </ul>
-            <ul>
-                Back End:
-                <li>Flask</li>
-                <li>Django</li>
-                <li>Python</li>
-                <li>Mysql</li>
-                <li>Docker</li>
-                <li>AWS</li>
-            </ul>
-        </div>
+            <Typography variant="h5" gutterBottom>
+                Tools and Technologies Used in current project:
+            </Typography>
+            <Grid container>
+                <Grid item md={6} xs={12}>
+                    <Typography variant="h6">Front End:</Typography>
+                    <List>
+                        {frontEndTech.map((tech,index) => (
+                            <ListItem key={index}>
+                                <ListItemText primary={tech} style={{ textAlign: 'center' }} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                    <Typography variant="h6">Back End:</Typography>
+                    <List>
+                    {backEndTech.map((tech,index) => (
+                            <ListItem key={index}>
+                                <ListItemText primary={tech} style={{ textAlign: 'center' }} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Grid>
+            </Grid>
+        </Container>
     );
 }
