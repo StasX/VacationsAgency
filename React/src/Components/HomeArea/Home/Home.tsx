1e976ec1421f9a0b09a77050ed63847d5fd4ca96
StasX <s.mestechkin@gmail.com>
2024-06-12 23:11:40 +0300

    refactor Home Element

--- React/src/Components/HomeArea/Home/Home.tsx
+++ React/src/Components/HomeArea/Home/Home.tsx
@@ -1,24 +1,27 @@
-import css from "./Home.module.css";
 import stat from "../../../Assets/img/stat.png"
 import { useTitle } from "../../../Hooks/useTitle";
 import { useRouteProtect } from "../../../Hooks/useRouteProtect";
+import { Box, Container, Grid, Typography } from "@mui/material";
 
 export function Home(): JSX.Element {
     useTitle("Home");
     useRouteProtect();
     return (
-        <div className={css.Home}>
-            <article>
-                <h1>Explanation about the system.</h1>
-                <p>
-                    <img src={stat} alt="Statistics" />
-                    Current our system display to administrators statistics about "Vacations agency".<br />
-                    Another our system "Vacation agency" allows to user to like / unlike vacations.
-                </p>
+        <Container maxWidth="xl" sx={{mt:5}}>
+            <Typography variant="h3">Explanation about the system.</Typography>
+            <Grid container sx={{mt:10}}>
+                <Grid item md={4} sm={12} >
+                    <Box sx={{margin:"auto"}}>
+                        <img src={stat} alt="Statistics" width={400} />
+                    </Box>
 
-            </article>
-
-
-        </div>
+                </Grid>
+                <Grid item md={8} sm={12}>
+                    
+                    <Typography>Another our system "Vacation agency" allows to user to like / unlike vacations.</Typography>
+                    <Typography>Current our system display to administrators statistics about "Vacations agency".</Typography>
+                </Grid>
+            </Grid>
+        </Container>
     );
 }
