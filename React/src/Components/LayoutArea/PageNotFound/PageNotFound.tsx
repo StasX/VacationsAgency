ff1f90dc946b94e19ce19a4a1b678699fdab6bd4
StasX <s.mestechkin@gmail.com>
2024-06-12 18:37:33 +0300

    Refactor Page Not Found

--- React/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
+++ React/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
@@ -1,18 +1,14 @@
 import css from "./PageNotFound.module.css";
 import { Link } from "react-router-dom";
 import notFound from "../../../Assets/img/404.jpg"
+import { Container, Typography } from "@mui/material";
 export function PageNotFound(): JSX.Element {
     return (
-        <div className={css.PageNotFound}>
-            <h1>
-                Page not found!
-            </h1>
+        <Container maxWidth="xs" className={css.PageNotFound}>
+            <Typography variant="h3" sx={{mt:10}}>Page not found!</Typography>
             <img src={notFound} alt="Page not found" />
-            <p>We sorry but requested page not found!<br />Please back to <Link to="/home">Home</Link>
-            </p>
-            <p>
-                <Link to="http://www.freepik.com">This image Designed by stories / Freepik</Link>
-            </p>
-        </div>
+            <Typography>We sorry but requested page not found!<br />Please back to <Link to="/home">Home</Link>
+            </Typography>
+        </Container>
     );
 }
